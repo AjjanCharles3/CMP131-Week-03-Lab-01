bbc069ada6cd8a5adfe4da977f89ff0aa1763dca
Ajjancharles3 <ajjan.charles@student.ccm.edu>
2026-09-09 14:22:34 -0400

    finished the assign

--- character_profile.py
+++ character_profile.py
@@ -1,6 +1,8 @@
 name="Kvothe"
 age=21
 quote=" I have talked to gods, loved women, and written songs that make the minstrels weep. You may have heard of me."
+job= Adventurer 
 print(name)
 print(age)
 print(quote)
+Print(job)
