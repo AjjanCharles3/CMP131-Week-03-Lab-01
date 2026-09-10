26137fa43e706ca1bbdb825c15675037ea196a52
Charles Ajjan <Ajjan.Charles@student.ccm.edu>
2026-09-10 16:19:02 -0400

    finished project

--- character_profile.py
+++ character_profile.py
@@ -1,15 +1,16 @@
-#charles Ajjan_CMP131-Week-03-Lab-01_charachter profile_09/09/2026
+#character profile_charles Ajjan_CMP131-Week-03-Lab-01_09/09/2026
 name="Kvothe"
 age=21
-quote=" I have talked to gods, loved women, and written songs that make the minstrels weep. You may have heard of me."
 job="Adventurer" 
 home="Neware"
 weakness="memory"
 strength="sympethy"
+quote='"I have talked to gods, loved women, and written songs that make the minstrels weep. You may have heard of me." - Kvothe'
 print(name)
 print(age)
-print(quote)
 print(job)
 print(home)
 print(weakness)
 print(strength)
+print("!!!!!NEWS!!!!!!")
+print(quote)
