82f5e569df52c3046191b09f257b786d1a04db92
Charles Ajjan <Ajjan.Charles@student.ccm.edu>
2026-09-10 16:37:30 -0400

    finished project

--- character_profile.py
+++ character_profile.py
@@ -1,16 +1,22 @@
 #character profile_charles Ajjan_CMP131-Week-03-Lab-01_09/09/2026
+print()
+Char="++Character"
+pro="Profile++"
 name="Kvothe"
 age=21
-job="Adventurer" 
-home="Neware"
-weakness="memory"
-strength="sympethy"
+job="I am an Adventurer and a Bartender and more" 
+weakness="My memory is my weakness"
+strength="Sympethy is my strength"
+home="The middle of Neware is where I call home"
 quote='"I have talked to gods, loved women, and written songs that make the minstrels weep. You may have heard of me." - Kvothe'
+print(Char, pro)
+print()
 print(name)
 print(age)
 print(job)
 print(home)
 print(weakness)
 print(strength)
-print("!!!!!NEWS!!!!!!")
+print()
+print("!!!!!--Character Quote--!!!!!!")
 print(quote)
